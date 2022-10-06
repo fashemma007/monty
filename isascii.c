c4975c1a523475dad97ca452f3bc1c3baec59fa2
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-06 09:29:34 +0100

    fixes:monty

--- isascii.c
+++ isascii.c
@@ -1,11 +1,10 @@
 #include "monty.h"
 /**
  * _isascii - checks if a given character is in ASCII
- * @char: character to check
+ * @chr: character to check
  * Return: int
- 
  */
 int _isascii(int chr)
 {
-  return((chr <= 127) && (chr >= 0));
+return ((chr <= 127) && (chr >= 0));
 }
